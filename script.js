9abc93e97a654a8cd64f3e753521e31ddea0f214
Jared Lynch <jaredlynch@protonmail.com>
2026-07-26 18:52:15 +1000

    Refactored JavaScript and added decimal functionality (don't yet have backspace functionality again)

--- script.js
+++ script.js
@@ -1,43 +1,81 @@
 // TODO:
 //  - Make it keyboard accessible
 //  - Display "ERROR" if too many characters on the screen (use toPrecision function)
-//  - Once all functions ar here, make a new document and refactor -- treat this
-//  as a portfolio item so produce it like one
+//  - Add backspace functionality
+//  - Add functionality that if num1, operator, and num2 are not null, if an
+//  operator is pressed, the result is calculated and the answer becomes num1,
+//  the operator becomes the next operator, and the answer and the operator are
+//  displayed on the screen
 const btn = document.querySelectorAll("button");
 const screen = document.querySelector(".screen");
 
 const OPERATORS = ["+", "-", "*", "/", "^"];
 
 function add(a, b) {
-  return a + b;
+  let answer = a + b;
+  if (answer % 1 !== 0) {
+    screen.textContent = answer.toFixed(2);
+    return answer;
+  } else {
+    screen.textContent = answer;
+    return answer;
+  }
 }
 function subtract(a, b) {
-  return a - b;
+  let answer = a - b;
+  if (answer % 1 !== 0) {
+    screen.textContent = answer.toFixed(2);
+    return answer;
+  } else {
+    screen.textContent = answer;
+    return answer;
+  }
 }
 
 function multiply(a, b) {
-  return a * b;
+  let answer = a * b;
+  if (answer % 1 !== 0) {
+    screen.textContent = answer.toFixed(2);
+    return answer;
+  } else {
+    screen.textContent = answer;
+    return answer;
+  }
 }
 
 function divide(a, b) {
-  return a / b;
+  if (b === 0) {
+    screen.textContent = "ERROR";
+    return null;
+  }
+  let answer = a / b;
+  if (answer % 1 !== 0) {
+    screen.textContent = answer.toFixed(2);
+    return answer;
+  } else {
+    screen.textContent = answer;
+    return answer;
+  }
 }
 
 function exponent(a, b) {
-  return a ** b;
+  let answer = a ** b;
+  if (answer % 1 !== 0) {
+    screen.textContent = answer.toFixed(2);
+    return answer;
+  } else {
+    screen.textContent = answer;
+    return answer;
+  }
 }
 
 let num1 = null;
 let num2 = null;
 let operator = null;
 
-// TODO:
-// - If a second operator is pressed when the `=` should be, calculate num1 and
-// num2 at that point, then add the operator to the next sum
-// - Don't allow multiple '.'s in one number
-// - Allow '.'s (more than one digit per number is needed first)
 btn.forEach((button) => {
   button.addEventListener("click", (e) => {
+    // FIXME: figure out what the clear buttons actually do on a calculator
     if (e.target.id === "clear") {
       num2 = null;
       operator = null;
@@ -47,95 +85,57 @@ btn.forEach((button) => {
       operator = null;
       num2 = null;
       screen.textContent = "";
-    } else if (num1 == null) {
-      // FIXME: this must be a number, and needs to handle multiple digits
-      num1 = e.target.id;
-      if (num1 === "<-") {
-        num1 = null;
-        screen.textContent = "";
-      } else {
-        screen.textContent = num1.toString();
+    } else if (num1 === null) {
+      // Check that the input is a number
+      if (!isNaN(parseInt(e.target.id))) {
+        num1 = e.target.id;
+        screen.textContent = num1;
       }
-    } else if (operator == null) {
-      // FIXME: this must be an operator
-      operator = e.target.id;
-      if (operator === "<-") {
-        num1 = null;
-        operator = null;
-        screen.textContent = "";
-      } else {
-        screen.textContent = `${num1.toString()} ${operator}`;
+    } else if (
+      (!isNaN(parseInt(e.target.id)) || e.target.id === ".") &&
+      operator === null
+    ) {
+      // Keep adding numbers to num1 while the input are numbers
+      num1 += e.target.id;
+      screen.textContent = num1;
+    } else if (operator === null) {
+      // Only add an input if an operator is selected
+      if (OPERATORS.includes(e.target.id)) {
+        operator = e.target.id;
+        screen.textContent = `${num1} ${operator}`;
       }
-    } else if (num2 == null) {
-      // FIXME: this must be a number, and needs to handle multiple digits
-      num2 = e.target.id;
-      if (num2 === "<-") {
-        operator = null;
-        num2 = null;
-        screen.textContent = num1.toString();
-      } else {
-        screen.textContent = `${num1.toString()} ${operator} ${num2.toString()}`;
+    } else if (num2 === null) {
+      // Check that input is a number
+      if (!isNaN(parseInt(e.target.id))) {
+        num2 = e.target.id;
+        screen.textContent = `${num1} ${operator} ${num2}`;
       }
-    } else if (e.target.id === "<-") {
-      num2 = null;
-      screen.textContent = `${num1.toString()} ${operator}`;
+    } else if (!isNaN(parseInt(e.target.id)) || e.target.id === ".") {
+      // Keep adding numbers to num2 while the input are numbers
+      num2 += e.target.id;
+      screen.textContent = `${num1} ${operator} ${num2}`;
     } else if (e.target.id === "=") {
-      const answer = calculate(num1, num2, operator);
-      screen.textContent = answer.toString();
-      // Reset
-      num1 = answer;
+      num1 = parseFloat(num1);
+      num2 = parseFloat(num2);
+      switch (operator) {
+        case "+":
+          num1 = add(num1, num2);
+          break;
+        case "-":
+          num1 = subtract(num1, num2);
+          break;
+        case "*":
+          num1 = multiply(num1, num2);
+          break;
+        case "/":
+          num1 = divide(num1, num2);
+          break;
+        case "^":
+          num1 = exponent(num1, num2);
+          break;
+      }
       num2 = null;
       operator = null;
     }
   });
 });
-
-function calculate(num1, num2, operator) {
-  // Change the numbers from strings to Floats to perform calculations
-  num1 = parseFloat(num1);
-  num2 = parseFloat(num2);
-  if (operator === "+") {
-    answer = num1 + num2;
-    // Do checks to see whether to dispay decimal point on the screen
-    if (answer % 1 !== 0) {
-      return answer.toFixed(2);
-    } else {
-      return answer;
-    }
-  } else if (operator === "-") {
-    answer = num1 - num2;
-    if (answer % 1 !== 0) {
-      return answer.toFixed(2);
-    } else {
-      return answer;
-    }
-  } else if (operator === "*") {
-    answer = num1 * num2;
-    if (answer % 1 !== 0) {
-      return answer.toFixed(2);
-    } else {
-      return answer;
-    }
-  } else if (operator === "/") {
-    answer = num1 / num2;
-    // This is any number divided by 0
-    if (answer == Infinity) {
-      return "ERROR";
-    }
-    if (answer % 1 !== 0) {
-      return answer.toFixed(2);
-    } else {
-      return answer;
-    }
-  } else if (operator === "^") {
-    answer = num1 ** num2;
-    if (answer == NaN) {
-      return "ERROR";
-    }
-    if (answer % 1 !== 0) {
-      return answer.toFixed(2);
-    } else {
-      return answer;
-    }
-  }
-}
